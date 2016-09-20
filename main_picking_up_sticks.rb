3abae23a218e7c6f4514f7894de15aece8d8dee9
Alexandra Craig <alexandra.craig@gmail.com>
2016-09-20 15:33:49 -0400

    Started main method. It request the 3 arguments to create an instance of SticksGame class - player1 name, player 2 name, starting stick count

--- main_picking_up_sticks.rb
+++ main_picking_up_sticks.rb
@@ -5,10 +5,10 @@ def main()
   puts "What is your name?"
   player1 = gets.chomp
   puts "Who is your opponent?"
-
-
-
-  bob = SticksGame.new("Bob", "Sally", 150)
+  player2 = gets.chomp
+  puts "How many sticks do you want to start with? \nChoose 1-20."
+  starting_stick_count = gets.chomp
+  game1 = SticksGame.new(player1, player2, starting_stick_count)
 end
 
 if __FILE__== $PROGRAM_NAME
