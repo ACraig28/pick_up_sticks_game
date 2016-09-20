b8a31205300e14a95fbf986c1a9bdea5b35a0859
Alexandra Craig <alexandra.craig@gmail.com>
2016-09-20 17:07:05 -0400

    added an error when picking up # of sticks would cause total to go into negatives

--- class_picking_up_sticks.rb
+++ class_picking_up_sticks.rb
@@ -1,3 +1,6 @@
+class NegativeSticksError < StandardError
+end
+
 class SticksGame
   attr_reader :player1, :player2, :starting_stick_count
 
@@ -5,5 +8,15 @@ class SticksGame
     @player1 = player1
     @player2 = player2
     @starting_stick_count = starting_stick_count
+  end
+
+  def picking_up_sticks(amount_picked_up)
+    sticks_left = @starting_stick_count -= amount_picked_up
+    if sticks_left < 0
+      @starting_stick_count = sticks_left += amount_picked_up
+      raise NegativeSticksError, "Error"
+    else
+      return sticks_left
+    end
   end
 end
--- main_picking_up_sticks.rb
+++ main_picking_up_sticks.rb
@@ -1,14 +1,29 @@
 require_relative 'class_picking_up_sticks'
 
 def main()
+  sticks_left = 0
   puts "Welcome to the picking up sticks!!"
   puts "What is your name?"
-  player1 = gets.chomp
+  player1 = gets.chomp.capitalize
   puts "Who is your opponent?"
-  player2 = gets.chomp
+  player2 = gets.chomp.capitalize
   puts "How many sticks do you want to start with? \nChoose 1-20."
-  starting_stick_count = gets.chomp
+  starting_stick_count = gets.chomp.to_i
   game1 = SticksGame.new(player1, player2, starting_stick_count)
+  until sticks_left == 1
+   begin
+      puts "#{player1}, how many sticks do you want to pick up? \nChoose 1-3."
+      amount_picked_up = gets.chomp.to_i
+      sticks_left = game1.picking_up_sticks(amount_picked_up)
+      puts "There are #{sticks_left} sticks left."
+      puts "#{player2}, how many sticks do you want to pick up? \nChoose 1-3."
+      amount_picked_up = gets.chomp.to_i
+      sticks_left = game1.picking_up_sticks(amount_picked_up)
+      puts "There are #{sticks_left} sticks left."
+    rescue NegativeSticksError
+      puts "You can not pick up that many sticks. Try again"
+    end
+  end
 end
 
 if __FILE__== $PROGRAM_NAME
